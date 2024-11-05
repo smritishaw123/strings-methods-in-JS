1f8279ef3c265ba71b5c011883f78a6b3c24a06c
smritishaw123 <smriti456shaw@gmail.com>
2024-11-05 09:06:51 +0530

    first commit

---
+++ string.js
@@ -0,0 +1,26 @@
+let msge = "     he llo   ";
+//example of trim method
+  let password = prompt("set your password");
+  let newPass = password.trim();
+ console.log(newPass);
+
+ 
+ //example touppercase and tolowercase strings
+ let name = "Aot College";
+ let msg = "error";
+ console.log(msg.toUppeCase())
+
+//example if indexof() method with argument
+ let sms ="ilovecoding";
+console.log(sms.indexOf("love"));
+
+//example of method chaining
+ let msgme = "  hello  ";
+ let newMsg = msgme.trim();
+ console.log(newMsg);
+ newMsg = newMsg.toUpperCase();
+ console.log(newMsg);
+
+//example of slice method
+let msg1 = "apnacollege";
+console.log(msg1.slice(0,4));
